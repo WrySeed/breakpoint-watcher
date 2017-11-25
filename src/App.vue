897bf8149a7e3077bcf1137e0d4e01665c9072fe
Olifant <olifant@live.fr>
2017-11-25 18:19:18 +0100

    Project setup

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <watcher-layout 
+      :currentBreakpoint="currentBreakpoint"/>
+  </div>
+</template>
+
+<script>
+import WatcherLayout from './components/WatcherLayout'
+
+const defaultBreakpoint = {
+  format: 'Desktop',
+  color: '#f2f2f2'
+}
+
+export default {
+  name: 'app',
+  components: {
+    WatcherLayout
+  },
+  data () {
+    return {
+      currentBreakpoint: {
+        format: defaultBreakpoint.format,
+        color: defaultBreakpoint.color
+      }
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+@import "./styles/reset.sass"
+@import "./styles/variables.sass"
+
+html
+  font-size: 62.5%
+
+#app
+  display: table
+  width: 100%
+  height: 100vh
+  background-color: $black-a5
+  font: $main-font
+</style>
---
+++ src/components/WatcherLayout.vue
@@ -0,0 +1,34 @@
+<template>
+    <section class="layout">
+        <div class="layout__column">
+            <h1 
+                class="layout__title"
+                :style="{ backgroundColor: currentBreakpoint.color }">{{ currentBreakpoint.format }}</h1>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+  name: 'WatcherLayout',
+  props: {
+    currentBreakpoint: Object
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+@import "../styles/variables.sass"
+
+.layout
+    display: table-cell
+    vertical-align: middle
+    &__column
+        width: 480px
+        margin: 0 auto
+        background-color: white
+        box-shadow: 0 5px 10px 0 rgba(0, 0, 0, .35)
+    &__title
+        padding: 2.2rem
+        font-size: 2rem
+</style>
