bddbd37c237fbf98a0059135de4e1557e1f6e195
WrySeed <wryseed@gmail.com>
2017-12-25 20:34:54 +0100

    Update background color on breakpoint change

--- src/components/Ocean.vue
+++ src/components/Ocean.vue
@@ -17,58 +17,49 @@ export default {
       required: true
     }
   },
+  data () {
+    return {
+      scene: new THREE.Scene(),
+      camera: new THREE.PerspectiveCamera(Config.fov, window.innerWidth / window.innerHeight, 0.1, 1000),
+      renderer: new THREE.WebGLRenderer({ antialias: true }),
+      light: new THREE.DirectionalLight(0xffffff, 0.75),
+      ocean: new Ocean(Config.geometry, Config.material, this.color)
+    }
+  },
+  methods: {
+    render: function () {
+      window.addEventListener('resize', this.resize)
+
+      this.ocean.moveWaves()
+
+      requestAnimationFrame(this.render)
+      this.renderer.render(this.scene, this.camera)
+    },
+    resize: function () {
+      this.$nextTick(() => {
+        let width = window.innerWidth
+        let height = window.innerHeight
+
+        this.ocean.setColor(this.color)
+
+        this.renderer.setSize(width, height)
+        this.camera.aspect = width / height
+        this.camera.updateProjectionMatrix()
+      })
+    }
+  },
   mounted: function () {
-    let scene, camera, renderer, light, ocean
+    this.camera.position.set(0, -25, 20)
+    this.camera.rotateX(Math.PI / 8)
+    this.light.position.set(15, -15, 500)
+    this.ocean.rotateZ(-Math.PI / 6)
 
-    const render = () => {
-      window.addEventListener('resize', resize, false)
+    document.getElementById('background').appendChild(this.renderer.domElement)
 
-      ocean.moveWaves()
+    this.scene.add(this.light, this.ocean)
 
-      requestAnimationFrame(render)
-      renderer.render(scene, camera)
-    }
-
-    const resize = () => {
-      renderer.setSize(window.innerWidth, window.innerWidth)
-      camera.aspect = window.innerWidth / window.innerWidth
-      camera.updateProjectionMatrix()
-    }
-
-    const init = () => {
-      // Ocean config & updatable values
-      let config = {
-        color: '#bada55',
-        fov: 60,
-        geometry: new THREE.PlaneGeometry(100, 100, 24, 24),
-        material: new THREE.MeshPhongMaterial({ shading: THREE.FlatShading })
-      }
-
-      // Scene setup
-      scene = new THREE.Scene()
-      camera = new THREE.PerspectiveCamera(config.fov, window.innerWidth / window.innerHeight, 0.1, 1000)
-      renderer = new THREE.WebGLRenderer({ antialias: true })
-      light = new THREE.DirectionalLight(0xffffff, 0.75)
-      ocean = new Ocean(config.geometry, config.material, config.color)
-
-      camera.position.set(0, -25, 20)
-      camera.rotation.set(Math.PI / 8, 0, 0)
-
-      light.position.set(15, -15, 500)
-
-      ocean.rotateZ(-Math.PI / 6)
-
-      // Add the scene to the DOM
-      document.getElementById('background').appendChild(renderer.domElement)
-
-      // Add objects to the scene
-      scene.add(light, ocean)
-
-      resize()
-      render()
-    }
-
-    init()
+    this.resize()
+    this.render()
   }
 }
 </script>
@@ -82,8 +73,5 @@ export default {
     height: 100%
     margin: 0
     overflow: hidden
-  canvas
-    width: 100%
-    height: 100%
 </style>
 
